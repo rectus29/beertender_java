bc95eadc89b2202da7ddcb1e57b6e93acb805396
rectus_29 <tenshi29GITHUB>
2018-07-02 18:15:29 +0200

    monday commit

---
+++ src/main/java/com/rectus29/beertender/web/page/product/ProductPage.java
@@ -0,0 +1,22 @@
+package com.rectus29.beertender.web.page.product;
+
+import com.rectus29.beertender.web.page.base.ProtectedPage;
+import org.apache.wicket.event.IEvent;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+public class ProductPage extends ProtectedPage {
+
+    public ProductPage(PageParameters parameters) {
+        super(parameters);
+    }
+
+    @Override
+    protected void onInitialize() {
+        super.onInitialize();
+    }
+
+    @Override
+    public void onEvent(IEvent event) {
+        super.onEvent(event);
+    }
+}
