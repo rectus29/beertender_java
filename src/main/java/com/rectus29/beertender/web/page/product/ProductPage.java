790e8b5fe1db946aa7dd094db279988746f7608d
rectus_29 <tenshi29GITHUB>
2018-07-03 09:53:52 +0200

    product page stub

--- src/main/java/com/rectus29/beertender/web/page/product/ProductPage.java
+++ src/main/java/com/rectus29/beertender/web/page/product/ProductPage.java
@@ -1,18 +1,30 @@
 package com.rectus29.beertender.web.page.product;
 
 import com.rectus29.beertender.web.page.base.ProtectedPage;
+import com.rectus29.beertender.web.security.error.ErrorPage;
 import org.apache.wicket.event.IEvent;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.util.string.StringValue;
 
 public class ProductPage extends ProtectedPage {
 
+    public static String PRODUCT_ID = "productId";
+    private StringValue productId;
+
+
     public ProductPage(PageParameters parameters) {
         super(parameters);
+        this.productId = getPageParameters().get(PRODUCT_ID);
+        if(this.productId.isNull() || this.productId.isEmpty()){
+            setResponsePage(ErrorPage.class, new PageParameters().add("ErrorCode", 422));
+        }
     }
 
     @Override
     protected void onInitialize() {
         super.onInitialize();
+
+
     }
 
     @Override
